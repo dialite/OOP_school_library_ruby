9bfc024831c7b0a0df28e861bf278b41ff843d59
dialite <raymondolisa01@gmail.com>
2023-02-02 16:06:32 +0100

    Implement review recommended fix of rnad to rand in person.rb

--- person.rb
+++ person.rb
@@ -6,7 +6,7 @@ class Person < Nameable
 
   def initialize(age, name = 'unknown', parent_permission: true)
     super()
-    @id = Random.rnad(1..1000)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
