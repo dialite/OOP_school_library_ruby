1f486c48e75e5132ec5a2de59aeccf848f1dd07b
dialite <raymondolisa01@gmail.com>
2023-02-03 07:56:08 +0100

    Create many-to-many relationship between person and book using intermediate class rental

--- book.rb
+++ book.rb
@@ -1,9 +1,18 @@
+require './rental'
+
 class Book
   attr_accessor :title, :author
+  attr_reader :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
+  end
+
+  def add_rental(date, author)
+    rental = Rental.new(date, self, person)
+    @rentals.push(rental)
   end
 
 end
