5c61f6718288d58b22eb2cbfa28a5aff3b57ebad
dialite <raymondolisa01@gmail.com>
2023-02-04 09:11:47 +0100

    Initialize app class and create run and menu method

--- app.rb
+++ app.rb
@@ -3,128 +3,56 @@ require './person'
 require './rental'
 require './student'
 require './teacher'
+require './classroom'
 
 class App
   def initialize
     @people = []
     @books = []
     @rentals = []
-  end
-
-  def list_books
-    @books.each do |book|
-      puts "Title: #{book.title} , Author: #{book.author}"
-    end
-  end
-
-  def list_persons
-    @people.each do |person|
-      case person
-      when Student
-        puts "[Student] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-      when Teacher
-        puts "[Teacher] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-      end
-    end
-  end
-
-  def create_book
-    print 'Title: '
-    title = gets.chomp
-    print 'Author: '
-    author = gets.chomp
-
-    @books << Book.new(title, author)
-    puts 'Book created successfully'
-  end
-
-  def create_person
-    print 'Do you want to create a student (1) or teacher (2)? [Input the number] : '
-    type = gets.chomp
-    print 'Age: '
-    age = gets.chomp.to_i
-    print 'Name: '
-    name = gets.chomp
-
-    case type
-    when '1'
-      print 'Has parent permission? [Y/N]: '
-      parent_permission = gets.chomp == 'y'
-      person = Student.new('classroom', age, name, parent_permission)
-    when '2'
-      print 'Specialization: '
-      specialization = gets.chomp
-      person = Teacher.new(age, specialization, name)
-    end
-
-    @people << person
-    puts 'Person created successfully'
-  end
-
-  def list_rentals
-    print 'ID of person: '
-    person_id = gets.chomp.to_i
-
-    puts 'Rentals:'
-    @rentals.each do |rental|
-      if rental.person.id == person_id
-        puts "Date: #{rental.date}, Book '#{rental.book.title}' By #{rental.person.name} "
-      end
-    end
-  end
-
-  def create_rental
-    puts 'Select abook from the following by number'
-    @books.each_with_index { |book, index| puts " #{index})  Title: #{book.title} , Author: #{book.author}" }
-
-    book_index = gets.chomp.to_i
-    book = @books[book_index]
-
-    puts 'select a person from the following list by number (not id)'
-    @people.each_with_index do |person, index|
-      case person
-      when Student
-        puts "#{index}) [Student] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-      when Teacher
-        puts "#{index}) [Teacher] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-      end
-    end
-
-    person_index = gets.chomp.to_i
-    person = @people[person_index]
-
-    print 'Date (YYYY-MM-DD): '
-    date = gets.chomp
-    @rentals << Rental.new(date, book, person)
-    puts 'Rental created.'
+    # @class = Classroom.new('Grade 9')
   end
 
   def run
-    options = { '1' => :list_books,
-                '2' => :list_persons,
-                '3' => :create_person,
-                '4' => :create_book,
-                '5' => :create_rental,
-                '6' => :list_rentals, '7' => :exit }
+    puts 'Welcome to School Library!'
+    loop do
+      menu
+      option = gets.chomp
+      break if option == '7'
 
-    puts ' '
-    puts 'Please select one of the following options:'
-    options.each { |key, value| puts "#{key} - #{value.to_s.capitalize.tr('_', ' ')}" }
-
-    option = gets.chomp
-    if options.key?(option)
-      send(options[option])
-    else
-      puts 'Invalid option, please try again.'
+      get_num(option)
     end
-
-    run
+    puts 'Thank you for using our Library!'
   end
-end
 
-def main
-  app = App.new
-  app.run
-end
+  def menu
+    puts 'Please choose an option by entering a number'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
 
-main
+  def get_num(option)
+    case option
+    when '1'
+      list_all_books
+    when '2'
+      list_all_people
+    when '3'
+      create_person
+    when '4'
+      create_book
+    when '5'
+      create_rental
+    when '6'
+      list_rentals_by_person_id
+    else
+      puts 'Please enter a number between 1 and 7'
+    end
+  end
+
+end
