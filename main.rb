ec0da4b9ab4baba2797e8fe0d548d9943b36adfb
dialite <raymondolisa01@gmail.com>
2023-02-04 14:29:11 +0100

    Modify main.rb with lists_rentals_by_person_id

--- main.rb
+++ main.rb
@@ -27,7 +27,7 @@ class MenuClass
       '3' => :create_person,
       '4' => :create_book,
       '5' => :create_rental,
-      '6' => :list_all_rentals,
+      '6' => :list_rentals_by_person_id,
       '7' => :exit
     }
     option = menu_list[user_input]
