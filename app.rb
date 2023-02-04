442d4113ef2eb912ac0641e01040bd15fd256cd9
dialite <raymondolisa01@gmail.com>
2023-02-04 11:10:13 +0100

    Create list of all people

--- app.rb
+++ app.rb
@@ -61,4 +61,10 @@ class App
     @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}"}
     sleep 0.75
   end
+
+  def list_all_people
+    puts 'There are no people yet! Kindly add a student or teacher.' if @people.empty?
+    @people.map { |person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"}
+    sleep 0.75
+  end
 end
